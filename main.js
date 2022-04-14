fc7fafc6ec8149ea8f0331bb336d7af44bac2a54
as-bowley <as.bowley@gmail.com>
2022-04-14 09:58:31 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+function add(val1, val2) {
+    const sum = val1 + val2;
+    return sum;
+}
+
+function subtract(val1, val2) {
+    const sum = val1 - val2;
+    return sum;
+}
+
+function multiply(val1, val2) {
+    const sum = val1 * val2;
+    return sum;
+}
+
+function divide(val1, val2) {
+    const sum = val1 / val2;
+    return sum;
+}
+
+function operate(oper, val1, val2) {
+    
+}
