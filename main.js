a4453972d04209f49b3fdb00fa3ce7fbb9289007
as-bowley <as.bowley@gmail.com>
2022-04-15 17:23:00 +0200

    add basic  functionality to calculator

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 const buttons = document.querySelector(".button-container");
 const display = document.querySelector(".display");
 const add = document.querySelector(".add");
-const subtract = document.querySelector(".subtract");
-const multiply = document.querySelector(".multiply");
-const divide = document.querySelector(".divide");
+const sub = document.querySelector(".sub");
+const multi = document.querySelector(".multi");
+const divi = document.querySelector(".divi");
 const equals = document.querySelector(".equals");
+const clear = document.querySelector(".clear");
 buttons.addEventListener("click", numInput, false);
+clear.addEventListener("click", clearFun, false);
+add.addEventListener("click", addition, false);
+equals.addEventListener("click", operate, false);
+sub.addEventListener("click", subtract, false);
+divi.addEventListener("click", divide, false);
+multi.addEventListener("click", multiply, false);
 
-const numValArr = [];
+let numValArr = [];
+let operArr = [];
+let val1 = 0;
+let val2 = 0;
+let result = 0;
+let oper = 0;
 
 function numInput(e) {
     if (e.target !== e.currentTarget && e.target.id == "num") {
@@ -18,30 +30,78 @@ function numInput(e) {
     }
 }
 
-function addFun(val1, val2) {
-    const sum = val1 + val2;
-    return sum;
+function addition() {
+   if (operArr.length === 0) {
+        val1 = numValArr.join('');
+        numValArr = [];
+        operArr.push('add');
+   } else {
+        val2 = numValArr.join('');
+        numValArr = [];
+        operate();
+   }
 }
 
-function subtractFun(val1, val2) {
-    const sum = val1 - val2;
-    return sum;
+function subtract() {
+    if (operArr.length === 0) {
+         val1 = numValArr.join('');
+         numValArr = [];
+         operArr.push('sub');
+    } else {
+         val2 = numValArr.join('');
+         numValArr = [];
+         operate();
+    }
+ }
+
+ function divide() {
+    if (operArr.length === 0) {
+         val1 = numValArr.join('');
+         numValArr = [];
+         operArr.push('divi');
+    } else {
+         val2 = numValArr.join('');
+         numValArr = [];
+         operate();
+    }
+ }
+
+ function multiply() {
+    if (operArr.length === 0) {
+         val1 = numValArr.join('');
+         numValArr = [];
+         operArr.push('multi');
+    } else {
+         val2 = numValArr.join('');
+         numValArr = [];
+         operate();
+    }
+ }
+
+function operate() {
+    if (operArr[0] == 'add') {
+        val2 = numValArr.join('');
+        result = parseInt(val1) + parseInt(val2);
+        display.textContent = result;
+    } else if (operArr[0] == 'sub') {
+        val2 = numValArr.join('');
+        result = parseInt(val1) - parseInt(val2);
+        display.textContent = result;
+    } else if (operArr[0] == 'divi') {
+        val2 = numValArr.join('');
+        result = parseInt(val2) / parseInt(val2);
+        display.textContent = result;
+    } else if (operArr[0] == 'multi') {
+        val2 = numValArr.join('');
+        result = parseInt(val1) * parseInt(val2);
+        display.textContent = result;
+    }
 }
 
-function multiplyFun(val1, val2) {
-    const sum = val1 * val2;
-    return sum;
+function clearFun() {
+    numValArr = [];
+    operArr = [];
+    sum = '';
+    display.textContent = '0';
 }
 
-function divideFun(val1, val2) {
-    const sum = val1 / val2;
-    return sum;
-}
-
-function operate(oper, val1, val2) {
-
-}
-
-function clear() {
-    display.textContent = 0;
-}
