68fd0d852226945b27e284f829c48ce4b1cf54a1
as-bowley <as.bowley@gmail.com>
2022-04-15 11:49:01 +0200

    add function to display numbers

--- main.js
+++ main.js
@@ -1,23 +1,47 @@
-function add(val1, val2) {
+const buttons = document.querySelector(".button-container");
+const display = document.querySelector(".display");
+const add = document.querySelector(".add");
+const subtract = document.querySelector(".subtract");
+const multiply = document.querySelector(".multiply");
+const divide = document.querySelector(".divide");
+const equals = document.querySelector(".equals");
+buttons.addEventListener("click", numInput, false);
+
+const numValArr = [];
+
+function numInput(e) {
+    if (e.target !== e.currentTarget && e.target.id == "num") {
+        let clickedItem = e.target.className;
+        numValArr.push(clickedItem);
+        display.textContent = numValArr.join('');
+        console.log(numValArr);
+    }
+}
+
+function addFun(val1, val2) {
     const sum = val1 + val2;
     return sum;
 }
 
-function subtract(val1, val2) {
+function subtractFun(val1, val2) {
     const sum = val1 - val2;
     return sum;
 }
 
-function multiply(val1, val2) {
+function multiplyFun(val1, val2) {
     const sum = val1 * val2;
     return sum;
 }
 
-function divide(val1, val2) {
+function divideFun(val1, val2) {
     const sum = val1 / val2;
     return sum;
 }
 
 function operate(oper, val1, val2) {
-    
+
+}
+
+function clear() {
+    display.textContent = 0;
 }
